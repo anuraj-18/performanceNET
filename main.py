224187751c6e8c4a7cf4ccf75f54ddab930bc97c
Shubham Anuraj <shubham.anuraj.s@gmail.com>
2018-02-24 14:41:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask,render_template
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return "Hello"
+  return "Hello world"
 
 if __name__ == '__main__':
   app.run()
