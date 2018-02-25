cdd89bdd539c90154023f7f919be4f56ada5f1ed
Shubham Anuraj <shubham.anuraj.s@gmail.com>
2018-02-25 12:05:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -266,7 +266,8 @@ def player_analysis(descriptors=None,name=None,avglast5=None,avglast5withteam=No
     if request.method=="POST":
         name=request.form["name"]
         name=name.split(" ")
-        name = name[0].upper() +" "+ name[1].title()
+        if len(name)==2:
+            name = name[0].upper() +" "+ name[1].title()
 
         country=request.form["country"]
         opp=request.form["opposition"]
